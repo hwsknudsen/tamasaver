d939eb0cf23771d8508e6edcb9a33ed49ec9da28
hwsknudsen <hwsknudsen@gmail.com>
2012-12-03 23:27:44 +0000

    animtor working with touch

--- TamaSaver/src/com/hwsknudsen/tamasaver/Main.java
+++ TamaSaver/src/com/hwsknudsen/tamasaver/Main.java
@@ -1,23 +1,52 @@
 package com.hwsknudsen.tamasaver;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.util.Log;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
-public class Main extends Activity {
+public class Main extends Activity{
+
 
 	public static final String PREFS_NAME = "MyPrefsFile";
 	private boolean firstrun;
-	ImageView iv;
+	public static ImageView iv;
+	public static long animationtime = 100;
+	private static int animationinprogress = 0 ;
+	
+	public static ArrayList<Actions> goingto = new ArrayList<Actions>();
+
+
+	Handler handler = new Handler(){
+
+		@Override
+		public void handleMessage(Message msg) {
+			if(animationinprogress == 0){
+				update();
+			}
+		}
+
+	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,22 +66,27 @@ public class Main extends Activity {
 			startActivity(intent);
 		}
 		iv = (ImageView) findViewById(R.id.imageView1);
-	   
-		Thread thread1 = new Thread(){
-	    	 public void run(){
-	             try {
-	                 sleep(1001); 
-	                 
-	                 
-	                 
-	             } catch (Exception e) {
-	                 e.printStackTrace();
-	             }
-	         }
-	     };
-	     thread1.start();
-	   
+	}
 
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+
+		Thread myThread=new Thread(new Runnable() {
+			public void run() {
+				while(true){
+					try {
+						handler.sendMessage(handler.obtainMessage());
+						Thread.sleep(animationtime);
+					}
+					catch (Throwable t) {
+					}
+				}
+			}
+		});
+
+		myThread.start();
 	}
 
 	@Override
@@ -74,21 +108,88 @@ public class Main extends Activity {
 	}
 
 	public void buttonClicked(View v) {
-//		RotateAnimation rotate = new RotateAnimation(0, 360);
-//		rotate.setDuration(500);
-//		iv.startAnimation(rotate);
-//		
-//		TranslateAnimation trans = new TranslateAnimation(0,100,0,100);
-//		trans.setDuration(500);
-//		iv.startAnimation(trans);
-//		
-		iv.setX(100);
-		iv.setY(100);
-		
-		//AlphaAnimation, AnimationSet, RotateAnimation, ScaleAnimation, TranslateAnimation 
+		animations.GoTO(new PointF(100,100));
+	}
 
-
+	public void buttonClicked2(View v) {
+		animations.GoTO(new PointF(200,200));
 	}
 
 
+	private void update() {
+		
+		if(!goingto.isEmpty()&animationinprogress==0){
+			animationinprogress = 1;
+			Actions thisanimation = goingto.get(0);
+			goingto.remove(0);
+			if (thisanimation.typeflag==0){
+				getAnimation(thisanimation).setAnimationListener(new AnimationListener(){
+					@Override
+					public void onAnimationEnd(Animation arg0) {
+						animationinprogress = 0;
+						update();
+					}
+					@Override
+					public void onAnimationRepeat(Animation arg0) {
+					}
+					@Override
+					public void onAnimationStart(Animation arg0) {
+					}
+					
+				});
+				iv.startAnimation(getAnimation(thisanimation));
+			}
+			else if (thisanimation.typeflag==2){
+				iv.setX(thisanimation.point.x);
+				iv.setY(thisanimation.point.y);
+				animationinprogress = 0;
+				update();
+			}
+			else if (thisanimation.typeflag==3){
+				//thisanimation.obA.start();
+			}
+		//animationtime = thisanimation.time;
+		}
+	}
+
+
+
+	private Animation getAnimation(Actions thisanimation) {
+		if(thisanimation.an!=null){
+			return thisanimation.an;
+		}
+		else if(thisanimation.as!=null){
+			return thisanimation.as;
+		}
+		else if(thisanimation.ra!=null){
+			return thisanimation.ra;
+		}
+		else if(thisanimation.sa!=null){
+			return thisanimation.sa;
+		}
+		else if(thisanimation.ta!=null){
+			return thisanimation.ta;
+		}
+		return null;
+	}
+	
+
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		//Log.e("hi","hi");
+		animations.GoTO(new PointF(event.getX(),event.getY()));
+	    return true;
+	}
+	
+	
+
+
+
+
+
+
+	//iv.setImageResource(R.drawable.path3891);
 }
+
+
