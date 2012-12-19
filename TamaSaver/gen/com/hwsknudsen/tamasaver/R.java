309d72305c7278dabe553b47b3c52e97eb5737d6
hwsknudsen <hwsknudsen@gmail.com>
2012-12-18 21:11:46 -0400

    change ordering

--- TamaSaver/gen/com/hwsknudsen/tamasaver/R.java
+++ TamaSaver/gen/com/hwsknudsen/tamasaver/R.java
@@ -23,20 +23,20 @@ public final class R {
     }
     public static final class id {
         public static final int Button01=0x7f070006;
-        public static final int ImageButton01=0x7f07000a;
-        public static final int ImageButton02=0x7f070009;
-        public static final int ImageButton03=0x7f07000c;
-        public static final int ImageButton04=0x7f070001;
-        public static final int ImageButton05=0x7f070002;
-        public static final int ImageButton06=0x7f070004;
-        public static final int ImageButton07=0x7f07000b;
-        public static final int LinearLayout01=0x7f070008;
+        public static final int ImageButton01=0x7f070003;
+        public static final int ImageButton02=0x7f070002;
+        public static final int ImageButton03=0x7f070005;
+        public static final int ImageButton04=0x7f07000b;
+        public static final int ImageButton05=0x7f07000a;
+        public static final int ImageButton06=0x7f07000c;
+        public static final int ImageButton07=0x7f070004;
+        public static final int ImageButton08=0x7f070009;
+        public static final int LinearLayout01=0x7f070001;
+        public static final int LinearLayout02=0x7f070008;
         public static final int button1=0x7f070007;
         public static final int checkBox1=0x7f07000f;
         public static final int checkBox2=0x7f070010;
-        public static final int imageButton2=0x7f070003;
-        public static final int imageView1=0x7f070005;
-        public static final int linearLayout3=0x7f070000;
+        public static final int imageView1=0x7f070000;
         public static final int menu_settings=0x7f070011;
         public static final int pager=0x7f07000d;
         public static final int textView1=0x7f07000e;
